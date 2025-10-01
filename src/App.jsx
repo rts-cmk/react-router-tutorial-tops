5d76bf13aaaabeeb4fc26db2a941dee40e793ce7
Tobias <neumsnn.tobias@gmail.com>
2025-10-01 10:57:46 +0200

    Made basic navigation setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import React from 'react'
+import { Routes, Route} from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import NotFound from './pages/PageNotFound'
+
+
 function App() {
   
 
   return (
     <>
-      
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </>
   )
 }
+
 
 export default App
